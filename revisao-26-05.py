ce70e4babec2a4cfbf6d48e82627c8b3f80461a2
Matheus Moreira <75402234+MatheusgMoreira@users.noreply.github.com>
2025-05-26 17:20:49 -0300

    aula 26-05

---
+++ revisao-26-05.py
@@ -0,0 +1,18 @@
+# 1. Declare 3 variáveis representando nome, idade e nota de um aluno. Depois, exiba esses dados com print.
+# 2. Crie expressões utilizando operadores aritméticos e relacionais.
+# 3. Peça uma nota e diga se o aluno foi aprovado (nota ≥ 6).
+# 4. Imprima os números de 1 a 10 usando um laço for.
+# 5. Crie uma função que receba dois números e retorne a soma.
+# 6. Peça ao usuário seu nome e idade e exiba em uma frase formatada.
+# 7. Crie uma lista com 5 frutas e imprima a segunda e a última fruta.
+# 8. Crie uma matriz 3x3 com números de 1 a 9 e imprima todos os elementos da diagonal principal.
+# 9. Peça um número ao usuário e use try/except para impedir erro caso ele digite texto.
+# 10. Peça para o usuário inserir uma frase e a modifique com pelo menos 3 métodos de string
+# 11. Use um while para pedir um número maior que zero. Se o número for negativo, use continue para pedir de novo.
+# 12. Crie um código com dois for que imprimam a tabuada de 1 a 5.
+# 13. Use range para imprimir os múltiplos de 3 entre 0 e 30.
+# 14. Crie uma tupla com os dias da semana e mostre a terça-feira.
+# 15. Peça uma idade e use uma operação ternária para dizer se é maior de idade.
+# 16. Crie uma função com dois parâmetros: nome e curso. Ela deve imprimir uma saudação.
+# 17. Altere a função anterior para que, ao invés de print, ela retorne a saudação.
+# 18. Crie um programa que cadastre nome, idade e curso de um aluno em um dicionário. A chave será o nome do aluno, o valor será outro dicionário com idadae e nota final. Exiba nome, idade e nota de cada aluno
